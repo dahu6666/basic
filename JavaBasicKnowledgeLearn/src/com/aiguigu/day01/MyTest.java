6e592cfa3073953926b1f7f54dd38799ce5e2d45
husijin <18926046930@163.com>
2020-11-23 07:45:08 +0800

    222

---
+++ JavaBasicKnowledgeLearn/src/com/aiguigu/day01/MyTest.java
@@ -0,0 +1,24 @@
+package com.aiguigu.day01;
+
+public class MyTest {
+	public static void main(String[] args) {
+		long q = 123L;
+		System.out.println("1111==" + q );
+		
+		float f = 12.3f;
+		int a1 = 1;
+		int a2 = 2;
+		String aa = (a1 + a2) + "";
+		String aa1 = a1+ "" + a2 ;
+		System.out.println("11111 =="+aa);
+		System.out.println("11112221 =="+aa1);
+		
+		char c1 = 5;
+		char c2 = '5';
+		int i1 = c1;
+		int i2 = c2;
+		System.out.println("c1 ==" + i1);
+		System.out.println("c2 ==" + i2);
+		
+	}
+}
